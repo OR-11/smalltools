10aeda0aff8dad098ec6bc189e8b3eb4d1ac9809
OR-11 <93813653+OR-11@users.noreply.github.com>
2026-02-14 00:37:40 +0900

    ルートディレクトリ完了

---
+++ script.js
@@ -0,0 +1,27 @@
+window.addEventListener("load", () => {
+    var IsRootPage;
+    const header = document.getElementById("header");
+
+    // ホームへ戻るボタンを追加
+    if (document.getElementById("root") != null) {
+        // root
+        IsRootPage = true;
+    }
+    else {
+        // 下位ページ
+        IsRootPage = false
+        const BackButton = document.createElement("a");
+        BackButton.setAttribute("href", "../");
+        BackButton.setAttribute("target", "_self");
+        BackButton.setAttribute("rel", "noopener noreferrer");
+        BackButton.setAttribute("class", "backToHome");
+        
+        BackButton.innerText = "< ホームへ戻る";
+
+        header.prepend(BackButton);
+    }
+});
+
+function getCurrentURL() {
+    return window.location.href;
+}
